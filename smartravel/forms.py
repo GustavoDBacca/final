ea65ad90f89bba8b2303808fa1e12132e2a3856b
Gustavson3D <gustavodrbacca@gmail.com>
2024-02-06 09:13:22 -0300

    atualizando.

--- smartravel/forms.py
+++ smartravel/forms.py
@@ -13,7 +13,7 @@ class CadastroLocalForm(forms.ModelForm):
     class Meta:
         model = Local
         cidade = forms.ModelChoiceField(queryset=Cidade.objects.all())
-        fields = ['nome_local', 'descricao', 'cidade', 'bairro', 'rua', 'telefone', 'email', 'tipo']
+        fields = ['nome_local', 'descricao', 'cidade', 'bairro', 'rua', 'telefone', 'email', 'tipo', 'id']
         tipo = forms.ModelChoiceField(queryset=Categorias.objects.all(), empty_label=None, widget=forms.Select(attrs={'class': 'form-control'}))
 
 class CadastroCidadeForm(forms.ModelForm):
